f18ef446ae1720339e7daf98fe7658ab9d82193b
Fayaz, Towfeeq <towfeeq.fayaz@rakuten.com>
2022-04-08 19:46:05 +0900

    initial commit

---
+++ Test.java
@@ -0,0 +1,12 @@
+
+ public class Test
+ {
+
+  public static void main(String args[])
+   {
+   System.out.println("Here it is the message  ");   
+    
+   System.out.println("\nThis is testing...!");   
+   }
+
+} 
